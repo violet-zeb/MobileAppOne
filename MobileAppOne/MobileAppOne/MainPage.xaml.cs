ee783563f06628b49d99ecd89f961156822b30d4
Stef PC <Stef PC@DESKTOP-VAA867M>
2022-02-02 15:48:53 +0100

    Add project files.

---
+++ MobileAppOne/MobileAppOne/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileAppOne
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Username = "02hero";
+        private const string Password = "predavanje3";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void InfoLabel_Clicked(object sender, EventArgs e)
+        {
+            if (UsernameEntry.Text == Username
+                && PasswordEntry.Text == Password)
+            {
+                InfoLabel.Text = "login successful";
+                InfoLabel.TextColor = Color.Green;
+            }
+            else
+            {
+                InfoLabel.Text = "login unsuccessful";
+                InfoLabel.TextColor = Color.Red;
+            }
+        }
+    }
+}
